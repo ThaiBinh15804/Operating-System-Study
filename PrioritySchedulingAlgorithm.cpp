eee015a81a707b51d6d4629cc46825a7b3f9065d
ThaiBinh <2001220401@hufi.edu.vn>
2023-10-02 09:48:38 +0700

    add MENU

--- PrioritySchedulingAlgorithm.cpp
+++ PrioritySchedulingAlgorithm.cpp
@@ -3,15 +3,15 @@
 #define MAX_priority 100
 
 struct process {
-    int process_no;
-    int arrival_time;
-    int burst_time;
-    int remaining_time;
-    int completion_time;
-    int waiting_time;
-    int turnaround_time;
-    int priority;
-    int original_priority;
+    int process_no;         //Mã tiến trình
+    int arrival_time;       //Thời gian tiến trình đến nhận CPU
+    int burst_time;         //Thời gian tiến trình cần để xử lí
+    int remaining_time;     //Thời gian còn lại (giá trị ban đầu = burst time)
+    int completion_time;    //Thời gian hoàn thành
+    int waiting_time;       //Thời gian chờ
+    int turnaround_time;    //Thời gian từ khi đến cho đến khi hoàn thành
+    int priority;           //Độ ưu tiên (có thể thay đổi để đánh dấu)
+    int original_priority;  //Độ ưu tiên gốc không đổi
 };
 
 void inputProcess(struct process *p, int process_no) {
@@ -63,7 +63,7 @@ void Priority_preemptive(process p[], int n)
         {
             if (last_process_no != p[selected_process].process_no)
             {
-                printf("P%d", p[selected_process].process_no);
+                printf("|P%d", p[selected_process].process_no);
                 last_process_no = p[selected_process].process_no;
             }
             p[selected_process].remaining_time --; //Thời gian thực hiện của tiến trình được chọn giảm 1
@@ -88,9 +88,10 @@ void Priority_preemptive(process p[], int n)
             }
         }
     }
+    printf("|");
 }
 
-void Priority_unpreemptive(process p[], int n)
+void Priority_non_preemptive(process p[], int n)
 {
     int remaining_processes = n;
     int current_time = 0;
@@ -123,7 +124,7 @@ void Priority_unpreemptive(process p[], int n)
             {
                 if (last_process_no != p[selected_process].process_no)
                 {
-                    printf("P%d", p[selected_process].process_no);
+                    printf("|P%d", p[selected_process].process_no);
                     last_process_no = p[selected_process].process_no;
                 }
                 p[selected_process].remaining_time --; //Thời gian thực hiện của tiến trình được chọn giảm 1
@@ -132,7 +133,7 @@ void Priority_unpreemptive(process p[], int n)
                 if (p[selected_process].remaining_time == 0)
                 {
                     remaining_processes --; //Tiến trình được chọn hoàn thành -> giảm số tiến trình còn lại
-                    printf("*");
+                    printf("*|");
                     current_time ++;
                     // Lưu lại thông tin về: completion_time, waiting_time, turnaround_time và đánh dấu tiến trình được chọn này đã hoàn thành
                     p[selected_process].completion_time = current_time;
@@ -151,7 +152,7 @@ void Priority_unpreemptive(process p[], int n)
     }
 }
 
-void displayProcessTable(struct process p[], int n) {
+void displayProcessTable(process p[], int n) {
     printf("\nProcessNo\tAT\tBT\tPri\tCT\tTAT\tWT\n");
     for (int i = 0; i < n; i++) {
         printf("P%d\t\t%d\t%d\t%d\t%d\t%d\t%d\n", p[i].process_no, p[i].arrival_time, p[i].burst_time,
@@ -159,12 +160,96 @@ void displayProcessTable(struct process p[], int n) {
     }
 }
 
+void ganGtri(process &a, process &b)
+{
+    a.process_no = b.process_no;
+    a.arrival_time = b.arrival_time;
+    a.burst_time = b.burst_time;
+    a.completion_time = b.completion_time;
+    a.original_priority = b.original_priority;
+    a.priority = b.priority;
+    a.remaining_time = b.remaining_time;
+    a.turnaround_time = b.turnaround_time;
+    a.waiting_time = b.waiting_time;
+}
+
+void ganMang(process a[], process b[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        ganGtri(a[i], b[i]);
+    }
+}
+
+void InKetQua(process p[], int n)
+{
+    displayProcessTable(p, n);
+
+    float avg_turnaround_time = 0, avg_waiting_time = 0;
+
+    for (int i = 0; i < n; i++) {
+        avg_turnaround_time += p[i].turnaround_time;
+        avg_waiting_time += p[i].waiting_time;
+    }
+
+    avg_turnaround_time /= n;
+    avg_waiting_time /= n;
+
+    printf("\nAverage Turnaround Time = %f\nAverage Waiting Time = %f\n", avg_turnaround_time, avg_waiting_time);
+
+}
+
+void MENU(process p[], int n)
+{
+
+    while (1!=0)
+    {
+        printf("\n<----- Lua chon che do uu tien ----->");
+        printf("\n1. Preemptive");
+        printf("\n2. Non-preemptive");
+        printf("\n0. Exit");
+        
+        int lc;
+        printf("\nNhap lua chon: "); 
+        scanf("%d", &lc);
+
+        switch (lc)
+        {
+        case 1:
+        {
+            process tmp[50];
+            ganMang(tmp, p, n);
+            printf("\n--- Bieu do the hien viec nhan CPU cua cac tien trinh ---\n\n");
+            Priority_preemptive(tmp, n);
+            printf("\n");
+            InKetQua(tmp, n);
+            break;
+        }
+        case 2:
+        {
+            process tmp[50];
+            ganMang(tmp, p, n);
+            printf("\n--- Bieu do the hien viec nhan CPU cua cac tien trinh ---\n\n");
+            Priority_non_preemptive(tmp, n);
+            printf("\n");
+            InKetQua(tmp, n);
+            break;
+        }
+        
+        case 0: return;
+
+
+        default: printf("\nVui long nhap lai!");
+            break;
+        }
+    }
+}
 
 
 int main() {
     int n;
 
-    printf("<-- Priority Scheduling Algorithm (Preemptive) -->\n");
+    printf("\n\n<-- Priority Scheduling Algorithm -->\n");
     printf("Enter Number of Processes: ");
     scanf("%d", &n);
 
@@ -174,24 +259,9 @@ int main() {
         inputProcess(&processes[i], i + 1);
     }
 
-    printf("\n--- Bieu do the hien viec nhan CPU cua cac tien trinh ---\n\n");
-    Priority_unpreemptive(processes, n);
-    printf("\n");
-
-    displayProcessTable(processes, n);
-
-    float avg_turnaround_time = 0, avg_waiting_time = 0;
-
-    for (int i = 0; i < n; i++) {
-        avg_turnaround_time += processes[i].turnaround_time;
-        avg_waiting_time += processes[i].waiting_time;
-    }
-
-    avg_turnaround_time /= n;
-    avg_waiting_time /= n;
-
-    printf("\nAverage Turnaround Time = %f\nAverage Waiting Time = %f\n", avg_turnaround_time, avg_waiting_time);
-
+    if (n > 0)
+        MENU(processes, n);
+    
 
     return 0;
 }
